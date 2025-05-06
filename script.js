1217d35d502ca386465eaabb1e69c35a39b806c2
Manish <xetrimohan12@gmail.com>
2025-05-06 15:42:41 +0100

    Initial commit - upload portfolio project

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+  function toggleDescription(button) {
+    const desc = button.parentElement.nextElementSibling;
+    if (desc.style.display === "none" || desc.style.display === "") {
+      desc.style.display = "block";
+      button.textContent = "Show Less";
+    } else {
+      desc.style.display = "none";
+      button.textContent = "Show More";
+    }
+  }
